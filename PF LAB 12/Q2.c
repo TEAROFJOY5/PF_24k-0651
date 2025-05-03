c54445984bdfc5d221995135cf0f9c517fff6efb
TEAROFJOY5 <turab653@gmail.com>
2025-05-03 17:47:31 +0500

    Create Q2.c

---
+++ PF LAB 12/Q2.c
@@ -0,0 +1,38 @@
+/*
+ Your function is given an array containing some numbers. Decrease the value of all odd
+numbers in the array by 1, so that the entire array contains only even numbers. Input array using
+malloc.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+void evenfun(int *arr, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        if ( arr[i] % 2 != 0)
+        {
+            arr[i]++;
+        }
+    }
+}
+
+
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&n);
+    int *arr= (int *)malloc(n * sizeof(int));
+    printf("\nEnter the elements of the array: ");
+    for(int i=0; i< n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    evenfun(arr,n);
+    printf("\n");
+    for ( int i=0; i<n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
