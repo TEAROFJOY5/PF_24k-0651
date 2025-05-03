2007bf58e1da5c86208e4d795526485c6c0642dd
TEAROFJOY5 <turab653@gmail.com>
2025-05-03 18:19:52 +0500

    Update Q4.c

--- PF LAB 12/Q4.c
+++ PF LAB 12/Q4.c
@@ -17,17 +17,17 @@ int main() {
         int N;
         scanf("%d", &N);
         int A[N];
-        int freq[10002] = {0};
+        int fre[10002] = {0};
         for (int i = 0; i < N; i++) {
             scanf("%d", &A[i]);
-            freq[A[i]]++;
+            fre[A[i]]++;
         }
-        int maxFreq = 0, result = 10002;
+        int maximumFre = 0, result = 10002;
         for (int i = 1; i <= 10001; i++) {
-            if (freq[i] > maxFreq) {
-                maxFreq = freq[i];
+            if (fre[i] > maximumFre) {
+                maximumFre = fre[i];
                 result = i;
-            } else if (freq[i] == maxFreq && i < result) {
+            } else if (fre[i] == maximumFre && i < result) {
                 result = i;
             }
         }
