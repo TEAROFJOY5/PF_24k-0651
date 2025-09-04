078e8d32e2a4653c4ed1e7749a01fbac9bfef7bb
TEAROFJOY5 <turab653@gmail.com>
2025-09-04 09:42:02 +0500

    Create pointerArrayInCPP.cpp

---
+++ firstcppCode/pointerArrayInCPP.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cout << "Enter the number of rows" << endl;
+    cin >> m;
+    cout << "Enter the number of columns" << endl;
+    cin >> n;
+    int **arr = new int*[m];
+    for(int i = 0; i < m; i++)
+    {
+        arr[i] = new int[n];
+    }
+    cout << "Enter the elements:" << endl;
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << "The 2D array is:" << endl;
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for(int i = 0; i < m; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
